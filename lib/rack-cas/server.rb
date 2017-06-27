5a615c8787af0868cdf2ce05f557a99eb22d97ec
David Gil <dgilperez@gmail.com>
2017-06-27 16:07:29 +0200

    :white_check_mark: :bug: fix bug

--- lib/rack-cas/server.rb
+++ lib/rack-cas/server.rb
@@ -13,7 +13,7 @@ module RackCAS
       base_params = {service: service_url}
       base_params[:renew] = true if RackCAS.config.renew?
 
-      url = RackCAS.config.login_url? ? RackCAS.config.login_url : @url.dup.append_path('login')
+      url = RackCAS.config.login_url? ? RackCAS::URL.parse(RackCAS.config.login_url) : @url.dup.append_path('login')
       url.add_params(base_params.merge(params))
     end
 
